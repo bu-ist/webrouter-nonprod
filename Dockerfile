7cfe79b193da450cf490b727c6f518447fb1a112
David King <dsmk@imp.bu.edu>
2018-01-20 10:55:18 -0500

    Initial version with the configuration from dsmk/docker-bufe-buedu

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# These are all defined in the base image
+#
+FROM dsmk/web-router-base
+
+# for now this is our split and everything below this is for a different location
+#
+# the final default landscape should be test
+ARG landscape=syst
+
+# These files remains in the landscape specific CodePipeline area.
+ADD landscape/${landscape}/sites.map /etc/nginx/sites.map
+ADD landscape/${landscape}/vars.sh /etc/nginx/vars.sh
+
---
+++ landscape/syst/vars.sh
@@ -0,0 +1,28 @@
+#
+# This file contains the various variables for the syst landscape.  It is a shorthand to keep container
+# definitions short.
+#
+
+export LOG_LEVEL=warn
+export LANDSCAPE=syst
+export ROBOTS_TXT=/home/robots.txt.test
+export BACKEND_REDIRECT=www-syst.bu.edu
+# backends until we get the F5 intermediaries in place
+export BACKEND_WEB_LEGACY=10.254.17.53
+export BACKEND_W3_CONTENT=www-syst.bu.edu
+export BACKEND_W3_MEDIA=www-syst.bu.edu
+export BACKEND_WP_APP=www-syst.bu.edu
+export BACKEND_WP_CONTENT=www-syst.bu.edu
+export BACKEND_PHPBIN=www-syst.bu.edu
+export BACKEND_DBIN=10.254.17.53
+export BACKEND_DJANGO=www-syst.bu.edu
+export BACKEND_DEGREE_ADVICE=www-syst.bu.edu
+#export BACKEND_REDIRECT=www-test.bu.edu
+#export BACKEND_W3_CONTENT=ist-w3-content-test.bu.edu:381
+#export BACKEND_W3_MEDIA=ist-w3-media-test.bu.edu:481
+#export BACKEND_WP_APP=ist-wp-app-test101.bu.edu
+#export BACKEND_WP_CONTENT=ist-wp-assets-test101.bu.edu
+#export BACKEND_PHPBIN=phpbin-test.bu.edu:181
+#export BACKEND_DBIN=dbin-test.bu.edu:181
+#export BACKEND_DJANGO=vsc66.bu.edu:197
+#export BACKEND_DEGREE_ADVICE=vsc08.bu.edu
