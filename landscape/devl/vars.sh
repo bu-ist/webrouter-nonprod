9a44f49aca6efeb995acdbc5fb37626d2e545cff
David King <dsmk@bu.edu>
2018-01-25 11:21:24 -0500

    Removed syst references in devl environment variables

--- landscape/devl/vars.sh
+++ landscape/devl/vars.sh
@@ -4,9 +4,9 @@
 #
 
 export LOG_LEVEL=warn
-export LANDSCAPE=syst
+export LANDSCAPE=devl
 export ROBOTS_TXT=/home/robots.txt.test
-export BACKEND_REDIRECT=www-syst.bu.edu
+export BACKEND_REDIRECT=www-devl.bu.edu
 # backends until we get the F5 intermediaries in place
 #export BACKEND_WEB_LEGACY=10.254.17.53
 #export BACKEND_WEB_CONTENT=ist-web-content-syst.bu.edu
@@ -14,10 +14,10 @@ export BACKEND_REDIRECT=www-syst.bu.edu
 #export BACKEND_WP_APP=www-syst.bu.edu
 #export BACKEND_WP_CONTENT=www-syst.bu.edu
 #export BACKEND_PHPBIN=ist-web-legacy-syst.bu.edu
-export BACKEND_PHPBIN=ist-web-legacy-syst.bu.edu
-export BACKEND_DBIN=ist-web-legacy-syst.bu.edu
-export BACKEND_DJANGO=ist-web-legacy-syst.bu.edu
-export BACKEND_DEGREE_ADVICE=ist-web-legacy-syst.bu.edu
+#export BACKEND_PHPBIN=ist-web-legacy-syst.bu.edu
+#export BACKEND_DBIN=ist-web-legacy-syst.bu.edu
+#export BACKEND_DJANGO=ist-web-legacy-syst.bu.edu
+#export BACKEND_DEGREE_ADVICE=ist-web-legacy-syst.bu.edu
 #export BACKEND_REDIRECT=www-test.bu.edu
 #export BACKEND_W3_CONTENT=ist-w3-content-test.bu.edu:381
 #export BACKEND_W3_MEDIA=ist-w3-media-test.bu.edu:481
