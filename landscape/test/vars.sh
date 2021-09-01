c13bbe7e342a8cb495db26fe71eee799b3791956
Ben Fenster <bfenster@bu.edu>
2021-09-01 03:43:11 -0600

    Remove ROBOTS_TXT override

--- landscape/test/vars.sh
+++ landscape/test/vars.sh
@@ -6,7 +6,7 @@
 export LOG_LEVEL=warn
 # ideally LANDSCAPE is automatically set by what is running it
 #export LANDSCAPE=devl
-export ROBOTS_TXT=/home/robots.txt.test
+#export ROBOTS_TXT=/home/robots.txt.test
 # backends until we get the F5 intermediaries in place
 #export BACKEND_REDIRECT=www-devl.bu.edu
 #export BACKEND_WEB_LEGACY=10.254.17.53
